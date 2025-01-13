b90c72aa6ff64aa3321381143b51bb04a6fa077c
mimiidjd <147587594+mimiidjd@users.noreply.github.com>
2025-01-13 01:42:21 +0100

    Ajout partie cours dans index.js

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 
 import { initBtns, changeState } from "./functions/quiz.js";
-
+import {changeState,  updateActiveStep} from "./functions/cours.js";
 export const stateContainer = document.querySelector("#stateContainer");
 
 //if (stateContainer.getAttribute("reloading") === "true"){
@@ -19,3 +19,19 @@ startBtn.addEventListener("click", () => {
 	changeState(document.querySelector("h1"), document.getElementById("stateContainer"), "question1");
 
 })
+
+//====== Cours ======
+
+//récup le boutton
+const nextButton = document.getElementById("#next");
+const title = document.querySelector("h1");
+let state = 1;
+const contentContainer = document.getElementById("cours");
+//listener du boutton
+document.addEventListener("click", function(event) {
+	if (event.target && event.target.id === "next") {
+		state++;
+		changeState(title, contentContainer, state);
+		updateActiveStep();
+	}
+});
